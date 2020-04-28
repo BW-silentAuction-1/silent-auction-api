2c1bfc955228450a2fb0453ea5989fad6a44967a
John <cjd8801@gmail.com>
2020-04-28 09:57:12 -0500

    bids and auctions built

--- auctions/auctionsRoutes.js
+++ auctions/auctionsRoutes.js
@@ -45,5 +45,80 @@ router.get('/', (req,res) => {
       .catch(err => res.status(500).json({message: "Error receiving auction"}));
   });
 
+  router.post("/", [item_price, validateAuction] , (req,res) => {
+    Auctions.add(req.auction).then(id => res.status(201).json(id[0]))
+      .catch(err => res.status(500).json({message: "Error receiving auction"}))
+  });
 
-module.exports.router;
+  router.put("/:id", [authOwner, validDate], (req,res) => {
+    const {user_id, id, ...rest} = req.body;
+    req.body = rest;
+    Bids.getBidsByAuction(req.params.id)
+      .then(bids => {
+       
+        if (bids.length) {
+          const {starting_price, name, date_starting, ...rest} = req.body;
+          req.body = rest;
+        }
+        Auctions.edit(req.params.id, req.body)
+        .then(records => res.status(201).json({records}))
+        .catch(err => res.status(500).json({message: "Error updating auction"}));
+      })
+      .catch(err => res.status(500).json({message: "Error receiving auction"}));
+  });
+
+  router.delete("/:id", [authOwner, validDate], (req,res) => {
+    Bids.getBidsByAuction(req.auction.id)
+    Auctions.remove(req.params.id)
+    .then(records => res.status(201).json({records}))
+    .catch(err => res.status(500).json({message: "Error deleting auction"}))
+  });
+
+  function item_price(req,res,next) {
+    if (req.decoded.seller) {
+      next();    
+    } else {
+      res.status(403).json({message: "You are unauthorized to perform this action."})
+    }
+  }
+
+  function authOwner(req,res,next) {
+    Auctions.getAuction(req.params.id)
+      .then(auction => {
+        if (auction) {
+          if (auction.user_id === req.decoded.subject) {
+            req.auction = auction;
+            next();
+          } else {
+            res.status(403).json({message: "You are unauthorized to perform this action."});
+          }
+        } else {
+          res.status(400).json({message: "Auction ID does not exist."})
+        }
+      })
+  }
+
+  function validateAuction(req, res, next) {
+    const auction = req.body;
+    if (auction) {
+      if (!auction.name || !auction.starting_price || !auction.date_ending || !auction.image || !auction.date_starting) {
+        res.status(400).json({message: "Name, starting price, end date, start date, and image URL is required."})
+      } else {
+        req.auction = {...auction, user_id: req.decoded.subject}
+        next();
+      }
+    } else {
+      res.status(400).json({message: "Body is required."})
+    }
+  }
+  
+  function validDate(req, res, next) {
+    let date = new Date().getTime();
+    let date_ending = new Date(req.auction.date_ending).getTime();
+    if (date < date_ending) {
+      next();
+    } else {
+        res.status(400).json({message: "Auction has ended."})
+    }
+  }
+  module.exports = router;
---
+++ bids/bids-model.js
@@ -0,0 +1,55 @@
+const db = require("../data/dbConfig");
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+  getBidsByAuction,
+  getBid,
+  add,
+  remove,
+  edit,
+  getLastBid,
+}
+
+function getBid(id) {
+  return db('auction_bids')
+    .where({id})
+    .first();
+}
+
+function getBidsByAuction(auction_id) {
+  return db('auction_bids')
+    .where({auction_id})
+    .orderBy('price')
+    .join('users', 'users.id', 'auction_bids.user_id')
+    .select('auction_bids.id', 'users.username', 'users.first_name', 'auction_bid.price', 'auction_bid.created_at')
+}
+
+function add(auction_id, token, price) {
+    var grab;
+    jwt.verify(token, secrets.jwtSecret, (error,decodedToken) => {
+            grab = decodedToken;
+    })
+    const bid = {auction_id: auction_id , price: price, user_id: grab.userId}
+  return db('auction_bids')
+    .insert(bid,['id'])
+}
+
+function getLastBid(auction_id) {
+  return db('auction_bids')
+    .where({auction_id})
+    .orderBy('price')
+    .first();
+}
+
+function edit(id, bid) {
+  return db('auction_bids')
+    .where({id})
+    .update({price: bid.price, created_at: bid.created_at})
+}
+
+function remove(id) {
+  return db('auction_bids')
+    .where({id})
+    .del();
+
+}
